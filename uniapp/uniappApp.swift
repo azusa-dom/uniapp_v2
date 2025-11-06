276764d10920964ca31e2844e3f50cf1d41c7ca3
可我真的什么都不会啊 <151507264+azusa-dom@users.noreply.github.com>
2025-11-06 21:29:44 +0800

    Initial Commit

---
+++ uniapp/uniappApp.swift
@@ -0,0 +1,17 @@
+//
+//  uniappApp.swift
+//  uniapp
+//
+//  Created by 748 on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct uniappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ uniappTests/uniappTests.swift
@@ -0,0 +1,16 @@
+//
+//  uniappTests.swift
+//  uniappTests
+//
+//  Created by 748 on 06/11/2025.
+//
+
+import Testing
+
+struct uniappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
